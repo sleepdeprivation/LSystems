7be05d26fe1ca9d8e809c1e7daa66bf8149050d3
sleepdeprivation <sleepdeprivation@users.noreply.github.com>
2015-02-26 09:48:30 -0800

    Update lsystem.js
    Commented LSystem object

--- lsystem.js
+++ lsystem.js
@@ -1,5 +1,10 @@
 /*
 //really, unecessary
+//wrote this because I though it would be a good idea to generate a new string
+//without allocating a new one and throwing away the old
+//Such a solution seems convoluted and unecessary tho
+//string as a linked list... hmm
+
 function listNode(data, next){
 	this.data = data || null;
 	this.next = next || null;
@@ -22,13 +27,18 @@ function list(data){
 */
 
 
-
+/*
+	Generates a string from the seed s using the production rules L
+	by its step() function.
+	It makes a new string, fills it using the production rules on the original,
+	and replaces the original.
+*/
 function Lsystem(s, L){
 
 	this.string = s;		//a string
 	this.productionRules = L;	//maps characters to strings
 
-
+	//produce the next string
 	this.step = function(){
 		var stringLength = this.string.length;
 		var rule;
@@ -40,6 +50,7 @@ function Lsystem(s, L){
 		this.string = templist;
 	}
 
+	//dump the current screen to the console
 	this.logAll = function(){
 		console.log(this.string);
 	}
